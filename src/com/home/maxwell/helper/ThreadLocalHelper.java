e52ccca07549f8b9fb65d1c79e0e154d082afd5f
maxewell yu <maxewell@hotmail.com.tw>
2014-07-01 17:01:04 +0800

    2014-07-01 Makeup ThreadLocalHelper

--- src/com/home/maxwell/helper/ThreadLocalHelper.java
+++ src/com/home/maxwell/helper/ThreadLocalHelper.java
@@ -1,10 +1,12 @@
 package com.home.maxwell.helper;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -12,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpRequest;
 
 import com.home.maxwell.ConstantKey;
@@ -21,7 +24,6 @@ import com.home.maxwell.interceptor.ApctlInterceptor;
 public class ThreadLocalHelper {
 	protected static Logger logger = LoggerFactory.getLogger(ThreadLocalHelper.class);
 	
-	//protected static final String ENV_RUNTIME_ATTR ="___ENV__RUNTIME";
 	protected static ThreadLocal<Map<String, Object>> threadLocal = new ThreadLocal<Map<String, Object>>(){
 		@Override
 		protected Map<String, Object> initialValue() {
@@ -31,20 +33,23 @@ public class ThreadLocalHelper {
 	
 	private ThreadLocalHelper(){}
 	
-	public static InputStream getResourceAsStream(String name){
-		/*
+	public static InputStream getResourceAsStream(String name) throws IOException{
 		Map<String, Object> map = (Map<String, Object>)threadLocal.get();
-		EnvirContext envCtx = (EnvirContext)map.get(ConstantKey.ENV_RUNTIME_ATTR);
+		ApplicationContext ctx = (ApplicationContext)map.get(ConstantKey.SPRING_CONTEXT_ATTR);
+		Resource resource = ctx.getResource(name);
 		
-		InputStream is = envCtx.getResourceAsStream(name);
-		
-		return is;
-		*/
-		return null;
+		if (resource != null){
+			return resource.getInputStream();
+		}else
+			return null;
 	}
 	
 	public static Set getResourcePaths(String name){
-		return null;
+		Map<String, Object> map = (Map<String, Object>)threadLocal.get();
+		HttpServletRequest req = (HttpServletRequest)map.get(ConstantKey.REQUEST_OBJ_ATTR);
+		ServletContext sctx = req.getSession().getServletContext();
+		
+		return sctx.getResourcePaths(name);
 	}
 	
 	public static Object getBean(String name){
